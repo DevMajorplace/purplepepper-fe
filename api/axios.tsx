917b42638375bf2861d2d9607905fe925134eaf4
ajh <ajh@majorplace.net>
2024-11-22 15:44:49 +0900

    Fix: API 서버 URL 변경

--- api/axios.tsx
+++ api/axios.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   //baseURL: "http://43.203.221.199:3000",
-  baseURL: "http://192.168.1.13:3000/",
+  baseURL: "https://majorinstareference.com/",
   withCredentials: true,
 });
 
