e1a02bcd121035012354e587965909e3e5a5d6a3
ajh <ajh@majorplace.net>
2024-11-25 11:13:05 +0900

    Fix: axios 헤더 추가

--- api/axios.tsx
+++ api/axios.tsx
@@ -4,6 +4,9 @@ const instance = axios.create({
   //baseURL: "http://43.203.221.199:3000",
   baseURL: "https://majorinstareference.com",
   withCredentials: true,
+  headers: {
+    "Access-Control-Allow-Origin": "https://majorinstareference.com",
+  },
 });
 
 export default instance;
