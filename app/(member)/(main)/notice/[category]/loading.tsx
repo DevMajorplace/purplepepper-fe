eb8a13a1e450d7cd33670bbb7ee79770a3f206d3
ajh <ajh@majorplace.net>
2024-11-27 18:04:10 +0900

    Remove: 공지사항 로딩페이지 삭제

--- app/(member)/(main)/notice/[category]/loading.tsx
+++
@@ -1,9 +0,0 @@
-export default function NoticeLoading() {
-  return (
-    <>
-      <div className="text-xl py-6 font-semibold cursor-pointer px-4 bg-[#ccc]">
-        <div className="h-[24px] w-[10%] bg-[#999]" />
-      </div>
-    </>
-  );
-}
