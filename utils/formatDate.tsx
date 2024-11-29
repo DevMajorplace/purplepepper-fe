6d9a49d1a9425eef7900303519b9e9b4d852e1c4
ajh <ajh@majorplace.net>
2024-11-29 14:12:11 +0900

    Fix: 데이트 포맷 수정

--- utils/formatDate.tsx
+++ utils/formatDate.tsx
@@ -3,13 +3,9 @@ export default function formatDate(date: string) {
   const utc = newDate.getTime() + newDate.getTimezoneOffset() * 60 * 1000;
   const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
   const kr_curr = new Date(utc + KR_TIME_DIFF);
-  console.log("toLocaleDateString ", kr_curr.toLocaleDateString());
-  console.log("toLocaleTimeString ", kr_curr.toLocaleTimeString());
-  console.log("toString ", kr_curr.toString());
-  console.log("toTimeString ", kr_curr.toTimeString());
-  console.log("toLocaleString ", kr_curr.toLocaleString());
-  //console.log("toLocaleDateString ", kr_curr.toTimeString().split(" ")[0]);
-  const format = `${kr_curr.toLocaleString()} ${kr_curr.toTimeString().split(" ")[0]}`;
+  const dateFormat = kr_curr.toLocaleDateString();
+  const timeFormat = kr_curr.toLocaleTimeString().split(" ")[0];
+  const format = `${dateFormat} ${timeFormat}`;
 
   return format;
 }
