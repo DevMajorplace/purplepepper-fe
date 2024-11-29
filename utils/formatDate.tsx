1a02624cb072b689b2b5d51acffea1d3f053a90c
ajh <ajh@majorplace.net>
2024-11-29 13:04:57 +0900

    Fix: 데이트 포맷 확인 콘솔 추가

--- utils/formatDate.tsx
+++ utils/formatDate.tsx
@@ -3,7 +3,9 @@ export default function formatDate(date: string) {
   const utc = newDate.getTime() + newDate.getTimezoneOffset() * 60 * 1000;
   const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
   const kr_curr = new Date(utc + KR_TIME_DIFF);
-  //console.log(kr_curr);
+  console.log(kr_curr.toLocaleString());
+  console.log(kr_curr.toTimeString());
+  console.log(kr_curr.toTimeString().split(" ")[0]);
   const format = `${kr_curr.toLocaleString()} ${kr_curr.toTimeString().split(" ")[0]}`;
 
   return format;
